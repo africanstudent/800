a44ef33e6df891d6662fa77b5e62807ae9da5fd6
africanstudent <rwamala@students.vu.ac.ug>
2024-07-05 01:06:24 +0300

    Delete 800 directory

--- 800/808FIX-Website/scripts/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const servicePrices = {
-        "11": { "audio": 20000, "network": 35000, "battery": 100000, "screen": 150000, "water": 200000 },
-        "11-pro": { "audio": 25500, "network": 40000, "battery": 105000, "screen": 159950, "water": 245500 },
-        "11-pro-max": { "audio": 31000, "network": 45000, "battery": 110000, "screen": 169900, "water": 291000 },
-        "12": { "audio": 36500, "network": 50000, "battery": 115000, "screen": 179850, "water": 336500 },
-        "12-mini": { "audio": 42000, "network": 55000, "battery": 120000, "screen": 189800, "water": 382000 },
-        "12-pro": { "audio": 47500, "network": 60000, "battery": ### Detailed Directory Structure Creation and Setup
