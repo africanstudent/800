b427c1c7b0d0f9802a7708f2bd6e25d9b82cbed0
Raymond Wamala Bazanye <raymond@Raymonds-MacBook-Pro.local>
2024-07-09 15:04:19 +0300

    Integrated Firebase and resolved issues

--- firebase.js
+++ firebase.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, ref, set } from "firebase/database";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCfo3vaVJ6Jc5CsA8T7quFFxpt_SuBhPh4",
   authDomain: "fix-dc8a0.firebaseapp.com",
@@ -19,5 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
 
 export { database, ref, set };
